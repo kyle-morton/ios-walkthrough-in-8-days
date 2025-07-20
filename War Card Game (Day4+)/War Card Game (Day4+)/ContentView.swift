63b9c1f1b6c420277da9d7cac794ac933d983124
Kyle Morton <mkmorton91@gmail.com>
2025-07-20 11:18:14 -0500

    Adding UI from video tutorial, keeping my stab at it

--- War Card Game (Day4+)/War Card Game (Day4+)/ContentView.swift
+++ War Card Game (Day4+)/War Card Game (Day4+)/ContentView.swift
@@ -9,6 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        ZStack { // 'depth-stack'
+            Image("background-plain")
+                .resizable() // if you leave this off, it'll show the image in it's full resolution (use this instead)
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                Spacer()
+                Image("button")
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundStyle(Color.white)
+                Spacer()
+            }
+                
+        }
+        
+    }
+}
+
+struct KMAttemptContentView: View {
+    var body: some View {
         VStack {
             HStack{
                 Spacer()
